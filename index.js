8bb49ffe23830537f5d7b405680395521d13908e
Tomer Amir <tamir@vonage.com>
2017-05-07 15:40:58 +0300

    Some cleanup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Load the TCP Library
 var net = require('net');
-var udp = require('./udp')
+var Udp = require('./udp')
 
 
 // Keep track of the chat clients
@@ -17,34 +17,39 @@ net.createServer(function (socket) {
 
     // Send a nice welcome message and announce
     socket.write("Welcome " + socket.name + "\n");
-    broadcast(socket.name + " joined the chat\n", socket);
+    // broadcast(socket.name + " joined the chat\n", socket);
 
     // Handle incoming messages from clients.
     socket.on('data', function (data) {
-        broadcast(socket.name + "> " + data, socket);
+        broadcast(data, socket);
     });
 
     // Remove the client from the list when it leaves
     socket.on('end', function () {
         clients.splice(clients.indexOf(socket), 1);
-        broadcast(socket.name + " left the chat.\n");
+        // broadcast(socket.name + " left the chat.\n");
     });
 
     // Send a message to all clients
     function broadcast(message, sender) {
+        console.log(sender.name + "> " + message);
+
         clients.forEach(function (client) {
             // Don't want to send it to sender
             if (client === sender) return;
             client.write(message);
         });
+
         // Log it to the server output too
         process.stdout.write(message)
     }
 
 }).listen(6000);
 
-const rtp = new udp(6001);
-const rtcp = new udp(6002);
+
+// UDP Sockets
+const rtp = new Udp(6001);
+const rtcp = new Udp(6002);
 
 // Put a friendly message on the terminal of the server.
 console.log("Chat server running at port 6000\n");
